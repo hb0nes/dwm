f68a319b8880d454242292f67397ab3cce0c384e
herman bnns <herman@bonnes.com>
2020-04-15 20:20:21 +0200

    Added some hotkeys for volume/brightness and maybe even a patch

--- config.h
+++ config.h
@@ -29,6 +29,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+static const char *downbrightness[] = { "light", "-U", "10", NULL };
+static const char *upbrightness[] = { "light", "-A", "10", NULL };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -36,8 +38,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",            NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",         NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "PrivMX",          NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "TelegramDesktop", NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -74,6 +78,8 @@ static Key keys[] = {
 	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
 	{ 0,                            XF86XK_AudioMute, spawn, {.v = mutevol } },
 	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+	{ MODKEY|ShiftMask,                            XK_Down, spawn, {.v = downbrightness   } },
+	{ MODKEY|ShiftMask,                            XK_Up, spawn, {.v = upbrightness   } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,                       XK_b,      togglebar,      {0} },
